d186636ea695dc952fd3d87ffff0d3bee9dda524
Jennifer Smith <jennifer@home>
2011-08-02 21:26:17 -0500

    Beginning of migration to capybara for csv features

--- capybara_features/step_definitions/search_steps.rb
+++ capybara_features/step_definitions/search_steps.rb
@@ -1,7 +1,28 @@
 require 'spec/spec_helper'
 
+include HpricotSearch
+
+When /^I search using a name of "([^\"]*)"$/ do |name|
+  Given %q{I am on the child search page}
+  When "I fill in \"#{name}\" for \"Name\""
+  And %q{I press "Search"}
+end
+
+When /^I select search result \#(\d+)$/ do |ordinal|
+  ordinal = ordinal.to_i - 1
+  
+	checkbox = page.all(:css, "p[@class=checkbox] input[@type='checkbox']")[0]
+  raise 'result row to select has not checkbox' if checkbox.nil?
+  check(checkbox[:id])
+end
+
 Then /^I should see "([^\"]*)" in the search results$/ do |value|
 
-  match = page.find('//a', :text => value)
+  rows = Hpricot(response.body).child_name
+
+  match = rows.find do |row|
+    row.search("a/text()")[0].to_plain_text == value
+  end
+
   raise Spec::Expectations::ExpectationNotMetError, "Could not find the value: #{value} in the search results" unless match
 end
